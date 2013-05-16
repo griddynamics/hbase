5698452bf6d8dc93f2430707dd98296d0abffe31
aleksgor <agorshkov@griddynamics.com>
2013-05-16 09:34:13 +0400

    format

--- hbase-server/src/test/java/org/apache/hadoop/hbase/metrics/TestMetricsHistogram.java
+++ hbase-server/src/test/java/org/apache/hadoop/hbase/metrics/TestMetricsHistogram.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
 package org.apache.hadoop.hbase.metrics;
 
 import java.util.Arrays;
@@ -6,7 +23,6 @@ import java.util.Random;
 import org.apache.hadoop.hbase.metrics.histogram.MetricsHistogram;
 import org.apache.hadoop.hbase.SmallTests;
 import org.apache.hadoop.metrics.MetricsRecord;
-import org.apache.hadoop.metrics.spi.MetricsRecordImpl;
 import com.yammer.metrics.stats.Snapshot;
 import org.junit.Assert;
 import org.junit.Test;
@@ -82,7 +98,7 @@ public class TestMetricsHistogram {
         + slop, data.length)];
     Assert.assertTrue(s.get95thPercentile() >= minAcceptableninetyFifth
         && s.get95thPercentile() <= maxAcceptableninetyFifth);
-    
+    // test the method pushMetric 
     MetricsRecord mr= mock(MetricsRecord.class);
     h.pushMetric(mr);
     verify(mr).setMetric("testHistogram_num_ops",1000L);
